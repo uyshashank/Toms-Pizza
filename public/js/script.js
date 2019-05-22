099c29daeb1991f63a6217ab2b6028674eaf2468
Shashank <uyshashank@gmail.com>
2019-05-22 16:52:41 +0530

    Add to Cart btn

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,14 @@
 function addToCart(id){
-    let pizza = id.name;
+    let pizza = id.name; 
+}
+function showBTN(spanThis){
+    let spanBlockValue = spanThis.attributes[1].value;    
+    let btn = "addToCartBtn"+spanBlockValue;
+    document.getElementById(btn).style.visibility = "visible";
+}
+
+function hideBTN(spanThis){
+    let spanBlockValue = spanThis.attributes[1].value;    
+    let btn = "addToCartBtn"+spanBlockValue;   
+    document.getElementById(btn).style.visibility = "hidden";
 }
