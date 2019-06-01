47c7aec35cc77e413afa807253989d0962cb872c
Shashank <uyshashank@gmail.com>
2019-06-01 22:49:31 +0530

    -

--- public/js/script.js
+++ public/js/script.js
@@ -3,18 +3,6 @@ function addToCart(id){
     console.log(item);
 }
 
-function showBTN(spanThis){
-    let spanBlockValue = spanThis.attributes[1].value;    
-    let btn = "addToCartBtn"+spanBlockValue;
-    document.getElementById(btn).style.visibility = "visible";
-}
-
-function hideBTN(spanThis){
-    let spanBlockValue = spanThis.attributes[1].value;    
-    let btn = "addToCartBtn"+spanBlockValue;   
-    document.getElementById(btn).style.visibility = "hidden";
-}
-
 function activate(btn1, btn2, btn3){        
     document.getElementById(btn1).classList.add('active');
     document.getElementById(btn2).classList.remove('active');
