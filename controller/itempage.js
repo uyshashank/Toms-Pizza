cddabfadab7850d6857cdcb9a5877d2c8b0309ce
shashank <uyshashank@gmail.com>
2019-06-04 13:26:29 +0530

    Small screen login logout feature done

--- controller/itempage.js
+++ controller/itempage.js
@@ -3,9 +3,10 @@ const db = require('../model/db');
 exports.IPDriver = (req, res) => {    
     let item = req.params.item;
     const logStatus = req.session.logStatus;
+    const userName = req.session.userName;
     db.loadItem(item)
     .then((itemDoc) => {
-        res.render('itempage/home', {itemDoc, logStatus});
+        res.render('itempage/home', {itemDoc, logStatus, userName});
     })
     .catch(err => console.log(err)); 
 }
